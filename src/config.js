26788f945a109bccc40cc02a36d0442fb1bb6e2b
6scars <zasalatol@o2.pl>
2026-09-13 09:06:19 +0200

    Maintenance: change config

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 const trimTrailingSlash = (value) => value.replace(/\/$/, "");
 
 export const BACKEND_URL = trimTrailingSlash(
-  import.meta.env?.VITE_BACKEND_URL || "http://localhost:3005"
+  import.meta.env?.VITE_BACKEND_URL || "http://site--norii-demo--cw7dcmybzfm4.code.run"
 );
 
 export const SUPABASE_STORAGE_URL = trimTrailingSlash(
-  import.meta.env?.VITE_SUPA_B_STOR || ""
+  import.meta.env?.VITE_SUPA_B_STOR || "https://uudncwmhipchzdmuocup.supabase.co/storage/v1/object/public/spotify/"
 );
